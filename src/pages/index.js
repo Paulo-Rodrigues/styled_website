8ce8c6eee99fc5ec10750f045e6f1ba833fa298e
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-31 10:54:52 -0300

    toggle function on sidebar

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <>
+     <Sidebar isOpen={isOpen} toggle={toggle} />
+     <Navbar toggle={toggle} />
+    </>
+  )
+}
